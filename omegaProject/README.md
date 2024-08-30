b97a24bfe6cdbeccaadd89a4bcee10da22cfc006
vladislav.bondarenko <vladislav.bondarenko@servicenow.com>
2024-08-29 22:27:17 -0700

    readme

--- omegaProject/README.md
+++ omegaProject/README.md
@@ -1,2 +1,5 @@
 # omegaProject
 A JavaScript project
+
+## W3 SCHOOLS
+- [Resources](https://www.w3schools.com/html/default.asp)
--- omegaProject/pages/index.js
+++ omegaProject/pages/index.js
@@ -4,12 +4,16 @@ export default function Home() {
         alert("click!");
     }
 
+    function consoleOut(){
+        console.log("hello world!")
+    }
+    
     return (
         <div>
             <h1>Hello Admin!</h1>
             <div id="dashboard">
                 <button onClick={alertMe}> Admin</button>
-                <button> Tasks</button>
+                <button onClick={consoleOut}> Tasks</button>
                 <button> Notes</button>
             </div>
         </div>
