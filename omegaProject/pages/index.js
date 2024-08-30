4e32156e205fdc93c4ff43f7938ca4796ea0ee27
vladislav.bondarenko <vladislav.bondarenko@servicenow.com>
2024-08-29 22:11:41 -0700

    second commit updating index

--- omegaProject/pages/index.js
+++ omegaProject/pages/index.js
@@ -1,9 +1,11 @@
 export default function Home() {
     return (
         <div>
-            <h1>Hello S</h1>
-            <div> id="Dashboard"
-                <button> Admin </button>
+            <h1>Hello Admin!</h1>
+            <div id="dashboard">
+                <button> Admin</button>
+                <button> Tasks</button>
+                <button> Notes</button>
             </div>
         </div>
     );
