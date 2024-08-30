29060423628cf0321b2ee2ad7e82997731ed9e29
vladislav.bondarenko <vladislav.bondarenko@servicenow.com>
2024-08-29 22:22:10 -0700

    alert

--- omegaProject/pages/index.js
+++ omegaProject/pages/index.js
@@ -1,9 +1,14 @@
 export default function Home() {
+    
+    function alertMe() {
+        alert("click!");
+    }
+
     return (
         <div>
             <h1>Hello Admin!</h1>
             <div id="dashboard">
-                <button> Admin</button>
+                <button onClick={alertMe}> Admin</button>
                 <button> Tasks</button>
                 <button> Notes</button>
             </div>
